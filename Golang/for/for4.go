635f912ed63b23d8e0bf4bba29c9c16d451bc8ac
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-03-06 02:13:26 +0900

    2022-03-06_1

--- Golang/for/for4.go
+++ Golang/for/for4.go
@@ -1,1 +1,21 @@
 package main
+import (
+		"fmt"
+		"bufio"
+		"os"
+		)
+
+func main(){
+	var len int
+	reader:=bufio.NewReader(os.Stdin)
+	writer:=bufio.NewWriter(os.Stdout)
+	fmt.Fscanln(reader, &len)
+	
+	var a,b int
+
+	for i:=0;i<len;i++{
+		fmt.Fscanln(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
+	}
+	writer.Flush()
+}
---
+++ Golang/for/for5.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+	var n int
+	fmt.Scanf("%d", &n)
+	for i:=1;i<=n;i++{
+		fmt.Printf("%d\n",i)
+	}
+}
