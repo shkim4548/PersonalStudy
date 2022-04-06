a713f1e87ba90c41b8a95f9edf2e40ad73d5a9b3
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-04-07 00:17:57 +0900

    2022-04-07

---
+++ dotnet/io/io11.cs
@@ -0,0 +1,10 @@
+using System;
+
+class io11{
+    static void main(String[] args){
+        int n;
+        n = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(n-543);
+    }
+}
---
+++ dotnet/io/io12.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class io12{
+    static void Main(String[] args){
+        int a,b,c;
+        a=int.Parse(Console.ReadLine());
+        b=int.Parse(Console.ReadLine());
+        c=int.Parse(Console.ReadLine());
+
+        Console.WriteLine((a+b)%c+"\n"+((a%c)+(b+c))%c+"\n"+(a*b)%c+"\n"+((a*b)*(b*c))%c);
+    }
+}
