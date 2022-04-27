e5d44ecbbc40e56d99f6de74c3794e6dfd9386ed
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-04-28 00:10:49 +0900

    22-04-28_1

--- Programmers/sql/leetsql1.sql
+++ Programmers/sql/leetsql1.sql
@@ -1,2 +1,14 @@
 ﻿--175. Combine Two Table
-SELECT
+i.	ALTER TABLE member ADD birth DATETIME]
+
+ALTER TABLE member ADD score int AFTER NUMBER
+
+ALTER TABLE member DROP birthday
+
+ALTER TABLE member MODIFY name VARCHAR(10)
+
+CREATE TABLE tb1(
+    `name`, RECORD_TYPE, DEFAULT NULL;
+)
+
+INSERT INTO tb1 record VALUES(DATA);
