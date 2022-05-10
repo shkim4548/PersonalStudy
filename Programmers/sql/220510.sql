d01cc7cd6584e163905d3c61f2e8fe0f4f53a427
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-05-10 12:50:49 +0900

    220510_1

---
+++ Programmers/sql/220510.sql
@@ -0,0 +1,31 @@
+﻿select * from tb;
+
+start transaction;
+
+delete from tb;
+
+select * from tb;
+
+ROLLBACK;
+
+select * from tb;
+
+COMMIT;
+
+set autocommit=0;
+
+insert into tb values('test', 555,555);
+
+select * from tb;
+
+rollback;
+
+set autocommit=1;
+
+create table tb1(
+    `number` varchar(30) default null,
+    `sales` varchar(30) default null,
+    `month` varchar(30) default null
+);
+
+load data infile '/var/html/t.csv' into table tbk fields terminated by ',';
--- Programmers/sql/leetsql1.sql
+++ Programmers/sql/leetsql1.sql
@@ -9,6 +9,103 @@ ALTER TABLE member MODIFY name VARCHAR(10)
 
 CREATE TABLE tb1(
     `name`, RECORD_TYPE, DEFAULT NULL;
+    ...
 )
 
 INSERT INTO tb1 record VALUES(DATA);
+
+
+create view v1 as select name, age from tb1L
+
+select * from v1;
+
+update v1 set name='주임 조수혁' where name='조수혁';
+
+select * from v1;
+
+select * from tb;
+
+create view v2
+as select tb.number, tb1L.name, tb.sales
+from tb
+join tb1L
+using(number)
+where tb.sales>=100;
+
+select * from v2;
+
+update tb set sales=777 where sales=54;
+
+select * from v2
+
+insert into v1 values('임시직 이정희', 18);
+
+select * from v1;
+
+select * from tb1L;
+
+create view v3 as select number, sales from tb where sales>=100;
+
+select * from v3;
+
+insert into v3 values('테스트',50);
+
+select number, sales from tb;
+
+create view v4 as select number, sales from tb where sales>=100 with check option;
+
+insert into v4 values('테스트',50);
+
+create or replace view v1 as select now();
+
+alter view v1 as select name, age from tb;
+
+drop view if exists v1;
+
+DELIMITER //
+create procedure pr1()
+BEGIN
+select * from tb;
+select * from tb1L;
+end
+//
+DELIMITER;
+
+DELIMITER //
+create procedure pr2(d INT)
+BEGIN
+select * from tb where sales >= d;
+END
+//
+DELIMITER;
+
+call pr2(200);
+
+DELIMITER //
+CREATE FUNCTION fu1(height INT) RETURNS DOUBLE
+BEGIN
+RETURN height * height *22 /10000;
+END
+//
+DELIMITER;
+
+DELIMITER //
+CREATE FUNCTION fu2() RETURNS DOUBLE
+BEGIN
+DECLARE r DOUBLE;
+SELECT AVG(sales) INTO r FROM tb;
+RETURN r;
+END
+//
+DELIMITER;
+
+DELIMITER
+//
+CREATE TRIGGER tr1 BEFORE DELETE ON tb FOR EACH ROW
+BEGIN
+INSERT INTO tb1M VALUES(OLD.number, OLD.name, OLD.age);
+END
+//
+DELIMITER;
+
+DROP TRIGGER tr1;
