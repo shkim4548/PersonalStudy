0b65c757f4168d7716e90a87ebabc6017e2e8146
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-05-15 01:23:57 +0900

    220515_1

--- Programmers/hashMap/hashMap1.cpp
+++ Programmers/hashMap/hashMap1.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #include<vector>
 #include<string>
@@ -8,21 +9,35 @@ using namespace std;
 //completion의 길이는 participant의 길이보다 1작다
 
 int main(void){
-    vector<string> participant={"mislav","stankov","mislav","ana"};
-    vector<string> completion={"stancov","ana","mislav"};
-    //vector<string> unfinished = {};
+    //3
+    //vector<string> participant={"mislav","stanko","mislav","ana"};
+    //vector<string> completion={"stanko","ana","mislav"};
 
-    std::vector<int>::iterator iter;
+    //2
+    vector<string> participant={"leo","kiki","eden"};
+    vector<string> completion={"eden","kiki"};
+    string answer;
+
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
 
-    for(int i=participant.size();i>=0;i--){
-        for(int j=completion.size();j>=0;j--){
-            if(participant[i]==completion[j]){
-                participant.pop_back();
-            }
+    for(auto loop : participant)
+        cout<<loop<<endl;
+    cout<<"\n";
+    for(auto loop : completion)
+        cout<<loop<<endl;
+
+    cout<<participant.capacity()<<"\n";
+    cout<<completion.capacity()<<"\n";
+
+    for(int i=0; i<participant.size(); i++){
+        if(i==completion.size())
+            answer=participant[participant.size()-1];
+        if(participant[i]!=completion[i]){
+            answer=participant[i];
+            break;
         }
     }
-    std::cout<<"\n"<<participant[0]<<"\n";
+    cout<<answer<<endl;
     return 0;
 }
