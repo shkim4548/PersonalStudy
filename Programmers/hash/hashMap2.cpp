186caacea690e6be5d9d36eb9d3feec2188c7e90
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-05-26 00:53:06 +0900

    220526_1
    이전에 한거긴 하지만 커밋이 늦어짐

--- Programmers/hash/hashMap2.cpp
+++ Programmers/hash/hashMap2.cpp
@@ -1,12 +1,31 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<algorithm>
 using namespace std;
 
 int main(){
-    vector<string> v1 ={"119","97674223", "1195524421"}
-    
-    //먼저 문자열을 3개씩 자르고
-    for(int i = 0;i<v1.size();i++){
-        v1[i].substr(i,3);
+    vector<string> v1 ={"119","97674223", "1195524421"};
+    vector<string> v2 ={"123","456","789"};
+    vector<string> v3 ={"12","123","1235","567","88"};
+    string sum;
+
+    //정렬을 조지면 제일 작은 크기의 문자열이 0번으로 무조건 온다
+    sort(v1.begin(), v1.end());
+    //따라서 정렬 후에
+
+    bool answer = true;
+    for(int i=0;i<v3.size();i++){
+        cout<<v1[i]<<endl;
+        if(sum.find(v3[0])!=string::npos){
+            answer=false;
+            sum.append(v3[i]);
+        }
+        else{
+            answer=true;
+            sum.append(v3[i]);
+        }
     }
+    cout<<sum<<endl;
+    cout<<answer<<endl;
 }
