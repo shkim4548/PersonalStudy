34316440565a24e2fc05797baa75c11292bdf09e
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-06-12 01:26:34 +0900

    220612

---
+++ solvedAC/classOneRemain/1152.cpp
@@ -0,0 +1,18 @@
+﻿#include"iostream"
+#include"string"
+
+int main(){
+
+    int cnt=0;
+    std::string str;
+    getline(std::cin, str);
+    for(int i=0;i<str.length();i++){
+        if(i!=0 && i!=str.length()-1)
+            if(str[i]==' ')
+                cnt++;
+    }
+    if(cnt==1 || str.length()==1)
+        std::cout<<1<<std::endl;
+    else
+        std::cout<<cnt+1<<std::endl;
+}
---
+++ solvedAC/classOneRemain/2675.cpp
@@ -0,0 +1,21 @@
+﻿#include"iostream"
+#include"string"
+using namespace std;
+
+int main(){
+    int n, cnt;
+    string str;
+
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>cnt;
+        getline(cin, str);
+        for(int j=1;j<str.length()+1;j++){
+            for(int k=0;k<cnt;k++){
+                cout<<str[j];
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
