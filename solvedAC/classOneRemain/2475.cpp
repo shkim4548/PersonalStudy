063e51524070b745d9b20d457fa9bf2c12383c6a
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-06-21 01:17:26 +0900

    220621_1

---
+++ solvedAC/classOneRemain/2475.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum, res, pow;
+    for(int i=0;i<5;i++){
+        cin>>n;
+        sum+=n*n;
+    }
+    res=sum%10;
+    cout<<res;
+    return 0;
+}
---
+++ solvedAC/classOneRemain/2908.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,res=0;
+    cin>>a;
+    cin>>b;
+    if(a>b){
+        res+=a%10;
+        res+=a%100-a%10;
+        res+=a/100;
+    }
+    else{
+        res+=b%10;
+        res+=b%100-b%10;
+        res+=b/100;
+    }
+    cout<<res;
+}
