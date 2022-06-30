d8a2410d8df7e064ef9d52fd16d515667ecfcb70
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-07-01 00:43:23 +0900

    220701_1

--- solvedAC/classTwo/solvedAc2/1085.cpp
+++ solvedAC/classTwo/solvedAc2/1085.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
 using namespace std;
 
-int main(void) {
-	int x, y, w, h, Axisx = 0, Axisy = 0;
+int main() {
+	int x, y, w, h, Axisx = 0, Axisy = 0, t;
+	int dis[4];
 	cin >> x >> y >> w >> h;
-	vector<int> dis;
-	
+
 	dis[0] = -(Axisx - x);
 	dis[1] = -(Axisy - y);
 	dis[2] = w - x;
 	dis[3] = h - y;
-
-	sort(dis.begin(), dis.end());
-	cout << dis[0] << '\n';
+	
+	for (int i = 0; i < 4; i++) {
+		for (int j = i; j < 4; j++) {
+			if (dis[i] < dis[j]) {
+				t = dis[i];
+				dis[i] = dis[j];
+				dis[j] = t;
+			}
+		}
+	}
+	cout << dis[3] << '\n';
 	return 0;
 }
