bbef56e7f6ac0e451dbf63e86a1f535512c38ead
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-07-23 02:41:14 +0900

    220723_3

--- solvedAC/classTwo/solvedAc2/15829.cpp
+++ solvedAC/classTwo/solvedAc2/15829.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 #include<string>
+#include<math.h>
 using namespace std;
 
 int main(void) {
 	int n, hash = 31;
+	long long hashed = 0;
+	char input[50];
+	//ÀÔ·ÂÀº ¿µ¹® ¼Ò¹®ÀÚ¸¸, ASCII 'a'==97
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> input[i];
+	}
+	for (int i = 0; i < n; i++) {
+		//»çÄ¢ ¿¬»ê ¿¡¼­ °ö¼ÀÀº µ¡¼Àº¸´Ù ¿ì¼±µË´Ï´Ù. ÀØÁö ¸¿½Ã´Ù °ýÈ£Ä¡±â
+		hashed += ((int)input[i] - 96) * (int)pow(hash, i);
+		//cout << (int)input[i] - 96 << " ";
+		//cout << (int)pow(hash, i) << " " << i;
+		//cout << hashed << '\n';
+	}
+	cout << hashed;
 }
---
+++ solvedAC/classTwo/solvedAc2/2292.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	//모든 방향으로 계차수열 6씩 차이난다. 한번에 6갈래로 나가야한다.
+	int n;
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		for(int j=1;)
+	}
+	return 0;
+}
