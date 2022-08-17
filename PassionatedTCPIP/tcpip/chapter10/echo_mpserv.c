042667a4c37ea32df2111f6c5903bca8a9f6a5be
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-08-18 01:49:27 +0900

    220818_1

--- PassionatedTCPIP/tcpip/chapter10/echo_mpserv.c
+++ PassionatedTCPIP/tcpip/chapter10/echo_mpserv.c
@@ -16,5 +16,75 @@ int main(int argc, char* argv[]){
     struct sockaddr_in serv_adr, clnt_adr;
 
     pid_t pid;
-    
+    struct sigaction act;
+    socklen_t adr_sz;
+    int str_len, state;
+    char buf[BUF_SIZE];
+
+    if(argc!=2){
+        printf("Usaage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    act.sa_handler=read_childproc;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags=0;
+    state=sigaction(SIGCHLD, &act, 0);
+    //위의 4줄은 좀비 프로세스의 생성을 막기위한 코드이다.
+
+    serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_adr.sin_port=htons(atoi(argv[1]));
+    //소켓 초기화
+
+    if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+        error_handling("bind() error");
+        //바인딩 실시 -> 서버와 클라이언트 연결
+    if(listen(serv_sock, 5)==-1)
+        error_handling("listen() error");
+        //
+
+
+    while(1){
+        adr_sz =sizeof(clnt_adr);
+        clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+        //accpt 함수를 이용하여 
+        if(clnt_sock==-1)
+            continue;
+        else
+            puts("new client connected...");
+        pid=fork();
+        if(pid==-1){
+            close(clnt_sock);
+            continue;
+        }
+        if(pid==0){
+            close(serv_sock);
+            while((str_len = read(clnt_sock, buf, BUF_SIZE))!=0)
+                write(clnt_sock, buf, str_len);
+            
+            close(clnt_sock);
+            puts("client disconnected...");
+            return 0;
+        }
+        else
+            close(clnt_sock);
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void read_childproc(int sig){
+    pid_t pid;
+    int status;
+    pid=waitpid(-1, &status, WNOHANG);
+    printf("removed proc id : %d \n", pid);
+}
+
+void error_handling(char* message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
