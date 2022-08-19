845927ae1c253dd7a05f7d442c883eca637f9b30
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-08-20 01:55:13 +0900

    220820_1

--- PassionatedTCPIP/tcpip/chapter11/echo_storeserv.c
+++ PassionatedTCPIP/tcpip/chapter11/echo_storeserv.c
@@ -1,1 +1,102 @@
-echo_storeserv.c
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/wait.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+#define BUF_SIZE 100
+//계속 잊어버리는데 전처리기에는 세미콜론 붙이는거 아니다
+
+void error_handling(char* message);
+void read_childproc(int sig);
+
+int main(int argc, char* argv[]){
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_adr, clnt_adr;
+    int fds[2];
+
+    pid_t pid;
+    struct sigaction act;
+    socklen_t adr_sz;
+    int str_len, state;
+    char buf[BUF_SIZE];
+    if(argc!=2){
+        printf("Usage : %s <port> \n", argv[0]);
+        exit(1);
+    }
+
+    act.sa_handler=read_childproc;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags=0;
+    state=sigaction(SIGCHLD, &act, 0);
+
+    serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_sock, 0, sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_adr.sin_port=htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+        error_handling("bind() error");
+    if(listen(serv_sock, 5)==-1)
+        error_handling("listen() error");
+
+    pipe(fds);
+    //여기서 파이프를 생성한다.
+    pid=fork();
+    //파일의 데이터 저장을 담당할 (자식)프로세스를 생성한다.
+    if(pid==0){
+        //위에서 생성한 자식 프로세스가 생성되는 영역
+        FILE* fp = fopen("echomsg.txt", "wt");
+        char msgbuf[BUF_SIZE];
+        int i, len;
+        for(i=0;i<10;i++){
+            len=read(fds[0], msgbuf, BUF_SIZE);
+            //파이프의 출구인 fds[0]으로 전달 되는 데이터를 읽는다.
+            fwrite((void*)msgbuf, 1, len, fp);
+            //읽어들이는 데이터를 저장한다.
+        }
+        fclose(fp);
+        return 0;
+    }
+    while(1){
+        adr_sz=sizeof(clnt_adr);
+        clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+        if(clnt_sock==-1)
+            continue;
+        else
+            puts("new client connected");
+        pid=fork();
+        //fork함수 호출로 생성되는 모든 자식 프로세스는 위의 파이프에서 생성된 파일디스크립터를 복사한다. 이를 통해 파이프의 입구인 fds[1]을 통해서 문자열을 전달 가능하다.
+        if(pid==0){
+            close(serv_sock);
+            while((str_len=read(clnt_sock, buf, BUF_SIZE))!=0){
+                write(clnt_sock, buf, str_len);
+                write(fds[1], buf, str_len);
+            }
+            close(clnt_sock);
+            puts("client disconnected...");
+            return 0;
+        }
+        else
+            close(clnt_sock);
+    }
+    close(serv_sock);
+    return 0;
+}
+
+void read_childproc(int sig){
+    pid_t pid;
+    int status;
+    pid=waitpid(-1, &status, WNOHANG);
+    printf("remove proc id : %d \n", pid);
+}
+
+void error_handling(char* message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ PassionatedTCPIP/tcpip/chapter12/select.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/time.h>
+#include<sys/select.h>
+#define BUF_SIZE 30
+
+int main(int argc, char* argv[]){
+    fd_set reads, temps;
+    int result, str_len;
+    char buf[BUF_SIZE];
+    struct timeval timeout;
+
+    FD_ZERO(&reads);
+    //fd_set 변수를 초기화한다.
+    FD_SET(0, &reads);
+    //파일 디스크립터 0의 위치를 1로 설정해준다 -> 표준 입력에 변화가 있는지 관심을 두겠다는 이유
+
+    //timeout.tv_sec = 5;
+    //timeout.tv_usec = 5000;
+
+    while(1){
+        temps=reads;
+        //미리 준비해둔 fd_set형 변수 reads의 내용을 변수 temps
+        timeout.tv_sec=5;
+        timeout.tv_usec=0;
+        result=select(1, &temps, 0, 0, &timeout);
+        if(result==-1){
+            puts("select() error");
+            break;
+        }
+        else if(result==0){
+            puts("Time-out!");
+        }
+        else{
+            if(FD_ISSET(0, &temps)){
+                str_len=read(0, buf, BUF_SIZE);
+                buf[str_len]=0;
+                printf("message from console: %s", buf);
+            }
+        }
+    }
+    return 0;
+}
