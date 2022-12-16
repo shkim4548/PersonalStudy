0ad204bdafb82c44d3e48903d82cb257d1843103
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-12-16 17:33:06 +0900

    221216-1

--- solvedAC/classTwo/solvedAc2/1929.cpp
+++ solvedAC/classTwo/solvedAc2/1929.cpp
@@ -1,21 +1,40 @@
 #include<iostream>
-//#include<vector>
+#include<math.h>
 using namespace std;
 
+int arr[1000001];
+
 int main(void) {
-	int start, end;
-	int numbers[1000] = { 0 };
-	cin >> start;
-	cin >> end;
-	for (int i = start; i <= end; i++) {
-		if (numbers[i] == 0)
-			continue;
-		for (int j = 2 * i; i <= end; i++)
-			numbers[j] = 0;
-	}
-	for (int i = 2; i <= end; i++) {
-		if (numbers[i] != 0)
-			cout << numbers[i] << '\n';
+	/*
+	int a, b, cnt = 0;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		cnt = 0;
+		for (int j = 2; j <= i; j++) {
+			if (i % j == 0)
+				cnt++;
+		}
+		if (cnt == 1)
+			cout << i << '\n';
 	}
 	return 0;
+	*/
+	//이상 2중 for문 풀이
+
+	int cnt = 0, a, b;
+	cin >> a >> b;
+	for (int i = 2; i < b; i++) {
+		arr[i] = i;
+	}
+	for (int i = 2; i < sqrt(b); i++) {
+		if (arr[i] == 0)
+			continue;
+		for (int j = 2 * i; j <= b; j += i) {
+			arr[j] = 0;
+		}
+	}
+	for (int i = a; i <= b; i++) {
+		if (arr[i] != 0)
+			cout << arr[i] << '\n';
+	}
 }
