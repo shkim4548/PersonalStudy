bc56640d0987106b94587443cb9561571d8eb4fb
shkim4548 <shkim4548@gmail.com>
2023-10-13 23:21:31 +0900

    201013 : 이진탐색과 vector파라미터 처리

--- solvedAC/classTwo/solvedAc2/1920.cpp
+++ solvedAC/classTwo/solvedAc2/1920.cpp
@@ -3,34 +3,51 @@
 #include <algorithm>
 using namespace std;
 
-/*-------------
+/*---------------------------------------------------------------------
 	이진탐색
----------------*/
+	vector를 파라미터로 주면 안됩니다!
+	vector를 함수의 인자로 줄 때 그냥 주면 복사를 해버려서 
+	vector의 길이만큼 시간이 걸립니다. 대신 참조형으로 주면 됩니다.
+-----------------------------------------------------------------------*/
+
+bool binarySearch(vector<int>& v, int key) {
+	int low = 0;
+	int high = v.size() - 1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (key == v[mid])
+			return true;
+
+		else if (key < v[mid])
+			high = mid - 1;
+		else if (key > v[mid])
+			low = mid + 1;
+	}
+	return false;
+}
 
 int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	int n, input, m;
 	vector<int> v1;
-	vector<int> v2;
 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> input;
 		v1.push_back(input);
 	}
+	sort(v1.begin(), v1.end());
 
 	cin >> m;
-	for (int j = 0; j < m; j++) {
+	for (int i = 0; i < m; i++) {
 		cin >> input;
-		v2.push_back(input);
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (v1[i] == v2[j]) {
-				cout << v1[i] << " " << v2[j] << '\n';
-				continue;
-			}
-		}
-		cout << 0 << '\n';
+		if (binarySearch(v1, input))
+			cout << 1 << '\n';
+		else
+			cout << 0 << '\n';
 	}
 }
