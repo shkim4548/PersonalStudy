062c08ef4de3d05a1a92b8c7ac989dad7774912d
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2023-10-17 18:10:06 +0900

    231017

--- solvedAC/classTwo/solvedAc2/10816.cpp
+++ solvedAC/classTwo/solvedAc2/10816.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-bool binarySearch(vector<int>& v, int key) {
-	int low = 0;
-	int high = v.size() - 1;
-
-	while (low <= high) {
-		int mid = (low + high) / 2;
-		if (key == v[mid])
-			return true;
-
-		else if (key < v[mid])
-			high = mid - 1;
-		else if (key > v[mid])
-			low = mid + 1;
-	}
-	return false;
-}
+/*-----------------------
+	unoredered_map
+	unordered_map과 딕셔너리의 차이를 알아보자
+-------------------------*/
 
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -26,22 +14,19 @@ int main(void) {
 	cout.tie(nullptr);
 
 	int n, m, input;
-	vector<int> standard, compare, res;
+	unordered_map<int, int> map;
 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> input;
-		standard.push_back(input);
+		map[input]++;
 	}
-
-	for (int i = 0; i < n; i++)
-		cout << standard[i] << '\n';
 
 	cin >> m;
 	for (int i = 0; i < m; i++) {
 		cin >> input;
-		compare.push_back(input);
+		cout << map[input] << ' ';
 	}
 
-	sort(standard.begin(), standard.end());
+	return 0;
 }
--- solvedAC/classTwo/solvedAc2/1966.cpp
+++ solvedAC/classTwo/solvedAc2/1966.cpp
@@ -5,23 +5,5 @@
 using namespace std;
 
 int main(void) {
-	int n, m, index, input;
-	vector<vector<pair<int, int>>> vp;
-
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> m >> index;
-		for (int j = 0; j < m; j++) {
-			cin >> input;
-			vp[j].push_back(make_pair(input, j + 1));
-		}
-		cout << i << '\n';
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < vp[i].size(); j++) {
-			cout << vp[i][j].first << " " << vp[i][j].second << ', ';
-		}
-		cout << '\n';
-	}
+	
 }
