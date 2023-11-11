de74997d7eaa8ff249e0f92bdf266794c8ee110e
shkim4548 <shkim4548@gmail.com>
2023-11-12 00:09:34 +0900

    231112

--- solvedAC/classThree/classThree/11724.cpp
+++ solvedAC/classThree/classThree/11724.cpp
@@ -1,35 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
 const int MAX = 1001;
-vector<bool> visited;
 vector<int> graph[MAX];
+bool visited[MAX];
+int cnt = 0;
 
-void dfs(int x) {
-	visited[x] = true;
-	for (int i = 0; i < graph[x].size(); i++) {
-		int y = graph[x][i];
-		if (!visited[y])
-			dfs(y);
+void bfs(int start) {
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+	cnt++;
+
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		//cout << x << ' ';
+		for (int i = 0; i < graph[x].size(); i++) {
+			int y = graph[x][i];
+			if (!visited[y]) {
+				q.push(y);
+				visited[y] = true;
+			}
+		}
 	}
 }
 
-int main() {
-	int N, M, u, v, sum = 0;
-	vector<int> vec;
+int main(void) {
+	int N, M, u, v;
 	cin >> N >> M;
-	for (int i = 1; i <= M; i++) {
+
+	for (int i = 0; i < M; i++) {
 		cin >> u >> v;
 		graph[u].push_back(v);
 		graph[v].push_back(u);
 	}
 
-	for (int i = 0; i < M; i++) {
-		//cout << graph[i].size() << '\n';
-		vec.push_back(graph[i].size());
+	for (int i = 1; i <= N; i++) {
+		if (visited[i] == false) {
+			bfs(i);
+		}
 	}
-	sort(vec.begin(), vec.end());
-	cout << vec[M-1] << '\n';
+	cout << cnt << '\n';
 }
