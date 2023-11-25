c3cfe75f878aa8160590c45ab78cb32fde715135
shkim4548 <shkim4548@gmail.com>
2023-11-26 01:03:32 +0900

    231126

--- solvedAC/classFour/1629.cpp
+++ solvedAC/classFour/1629.cpp
@@ -2,8 +2,45 @@
 #include <vector>
 using namespace std;
 
+/*
+	곱셈문제
+	- (A^B)%C를 구해라
+	- 분할 정복을 이용한 거듭제곱 계산
+*/
 
+int involution(long long base, long long exponent, long long mod) 
+{
 
-int main(void) {
+	// 탈출 조건
+	if (exponent == 1)
+		return base % mod;
+	else if (base == 0)
+		return 1;
 
+	long long newBase;
+
+	// 거듭제곱을 실행하는 부분
+	// 짝수인 경우
+	if (exponent % 2 == 0) {
+		newBase = involution(base, exponent / 2, mod) % mod;
+		return newBase * newBase % mod;
+	}
+	// 홀수인 경우
+	else 
+	{
+		newBase = involution(base, (exponent - 1) / 2, mod) % mod;
+		return (newBase * newBase) % mod * base % mod;
+	}
+}
+
+int main(void) 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << involution(a, b, c) << '\n';
 }
--- solvedAC/classThree/classThree/9095.cpp
+++ solvedAC/classThree/classThree/9095.cpp
@@ -2,8 +2,32 @@
 #include <vector>
 using namespace std;
 
+const int MAX = 12;
+int cnt = 1, n;
+vector<int> memo(MAX);
+
+int sum(int start) 
+{
+	if (start <= 1)
+		return 1;
+
+	if (start == 2)
+		return 2;
+
+	return sum(start - 1) + sum(start - 2) + sum(start - 3);
+}
+
 int main(void) {
-	int n;
-	vector<int> memo;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int start;
 	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> start;
+		cout << sum(start) << '\n';
+	}
+	return 0;
 }
