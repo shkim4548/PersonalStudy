6ec75413da06cb50496e968389c1e00c0f5ef1f7
shkim4548 <shkim4548@gmail.com>
2023-12-07 00:53:30 +0900

    231207

--- solvedAC/classFour/1916.cpp
+++ solvedAC/classFour/1916.cpp
@@ -1,14 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
 /*
 	다익스트라 알고리즘
-	- 
+	- BFS에 우선순위 큐를 사용하여 누적치를 갱신한다
+	- 이를 통해 가중치가 최소치인 경로를 찾아간다.
 */
+
+const int INF = 100000000;
+const int MAX = 1001;
+vector<pair<int, int>> nodes[MAX];
+int dist[MAX];
+
+void dijkstra(int start)
+{
+	priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
+	pq.push(make_pair(0, start));	// dist를 짧은 거리 기준으로 정렬하기 위해 파라미터를 0을 앞에 배치, 앞에가 정렬기준
+	while (!pq.empty())
+	{
+		// 우선순위큐는 내림차순 정렬이다
+		int cost = pq.top().first;
+		int here = pq.top().second;
+		pq.pop();
+
+		// 이미 최단거리 정보가 있다면 생략한다
+		if (dist[here] < cost)
+			continue;
+
+		for (int i = 0; i < nodes[here].size(); i++)
+		{
+			int viaCost = cost + nodes[here][i].first;
+
+			// here을 경유해서 가는게 빠르면 dist 갱신 후 push
+			if (viaCost < dist[nodes[here][i].second])
+			{
+				dist[nodes[here][i].second] = viaCost;
+				// 내림차순 정렬을 위해 음수를 사용
+				pq.push({ viaCost, nodes[here][i].second });
+			}
+		}
+	}
+}
 
 int main(void)
 {
+	int n, m;
+	cin >> n >> m;
 
+	// 초기화, 모든 거리를 우선 INF로 하여 이동을 불가하게 한다.
+	for (int i = 1; i <= n; i++)
+	{
+		dist[i] = INF;
+	}
+
+	// 그래프를 입력받아 노드를 채운다.
+	for (int i = 1; i <= m; i++) 
+	{
+		int start, end, cost;
+		cin >> start >> end >> cost;
+		nodes[start].push_back(make_pair(cost, end));
+	}
+	int startPoint, endPoint;
+	cin >> startPoint >> endPoint;
+	// end를 넣을 필요는 없는게 전체 순회 후 최소 비용을 구한다 -> 마지막에 dist[endPoint];
+	dijkstra(startPoint);
+	cout << dist[endPoint] << '\n';
 }
--- solvedAC/classThree/classThree/11723.cpp
+++ solvedAC/classThree/classThree/11723.cpp
@@ -3,38 +3,61 @@
 #include <string>
 using namespace std;
 
+/*
+	비트 마스킹
+	- 패킷 조립등을 위해 자주 사용하는 기법이니 반드시 숙지할 것
+	- https://gyoogle.dev/blog/algorithm/BitMask.html
+*/
+
 int main(void) 
 {
-	int n, bitmask = 20, input;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int bitmask = 0;
+	int n, input;
 	string calculate;
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> calculate;
-		cin >> input;
 		if (calculate == "add")
 		{
-			bitmask << input;
+			cin >> input;
+			// input 번째 자리수를 1로 채운다
+			bitmask |= (1 << input);
 		}
 		else if (calculate == "remove")
 		{
-
+			cin >> input;
+			// input 번째 자리수를 반전하여 비트곱한다 -> 반드시 0이 된다
+			bitmask &= ~(1 << input);
 		}
 		else if (calculate == "check")
 		{
-
+			cin >> input;
+			// 해당 위치의 비트마스크가 true인지를 확인한다.
+			if (bitmask & (1 << input))
+				cout << 1 << '\n';
+			else
+				cout << 0 << '\n';
 		}
 		else if (calculate == "toggle")
 		{
-
+			cin >> input;
+			bitmask ^= (1 << input);
 		}
 		else if (calculate == "all")
 		{
-
+			//cin >> input;
+			// -1을 이용해서 전부 뒤집어버린다.
+			bitmask = (1 << 21) - 1;
 		}
 		else if (calculate == "empty")
 		{
-
+			//cin >> input;
+			bitmask = 0;
 		}
 	}
 }
